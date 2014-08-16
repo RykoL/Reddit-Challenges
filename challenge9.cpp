406e85d165d601a7e2822e18e13e10edb6ee196f
RykoL <rico.lang@vr-web.de>
2014-08-16 16:01:28 +0200

    added challenge9

---
+++ challenge9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+
+int main(int argc, char const *argv[])
+{
+	std::vector<int> digits;
+	std::string input;
+	std::string temp;
+
+	std::cout<< ">> ";
+	std::cin >> input;
+
+	for(char c : input)
+	{
+		if(std::isdigit(c) != 0)
+		{
+			temp = c;
+			digits.push_back(std::stoi(temp));
+		}
+	}
+
+	std::sort(digits.begin(), digits.end(),std::less<int>());
+
+	for(int digit : digits)
+	{
+		std::cout<< digit <<std::endl;
+	}
+	return 0;
+}
